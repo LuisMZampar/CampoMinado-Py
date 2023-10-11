3b8bb12919fedc08f5d2ce8e26b528bc4e86fbda
LuisMZampar <rm550531@fiap.com.br>
2023-10-11 20:02:20 -0300

    Terminando o projeto

--- CampoMinado.py
+++ CampoMinado.py
@@ -18,25 +18,24 @@ def imprime(mat):
                 st = st + " " + str(valor)
         print(st)
 
+
 def coloca_bomba(mat):
     qtd_bomba = 0
     while qtd_bomba < 10:
-        #Sortear uma posição da matriz 
-        #Se nao tiver bomba, coloco a bomba
-        #E incremento qtd_bomba
-        i = random.randint(0,9)
-        j = random.randint(0,9)
+        i = random.randint(0, 9)
+        j = random.randint(0, 9)
         if mat[i][j] == 0:
             mat[i][j] = -1
-            qtd_bomba = qtd_bomba + 1
+            qtd_bomba = qtd_bomba + 1        
 
-#retorna uma lista contendo tuplas que indicam as posições vizinhas de (lin, col)
+#retorna uma lista contendo tuplas que 
+# indicam as posicoes vizinhas de (lin, col) 
 def retorna_vizinhos(lin, col):
     lista = []
     if lin > 0 and col > 0:
         lista.append([lin-1, col-1])
     if lin > 0:
-        lista.append([lin-1, col])
+        lista.append([lin-1, col])        
     if lin > 0 and col < 9:
         lista.append([lin-1, col+1])
 
@@ -54,7 +53,7 @@ def retorna_vizinhos(lin, col):
     
     return lista
 
-def coloca_numero(mat):
+def coloca_numeros(mat):
     for i in range(10):
         for j in range(10):
             if mat[i][j] == 0:
@@ -63,31 +62,50 @@ def coloca_numero(mat):
                     if mat[x][y] == -1:
                         mat[i][j] = mat[i][j] + 1
 
+def mostra_campo(mtab, mesp):
+    for i in range(10):
+        st = ""
+        for j in range(10):
+            if mesp[i][j] == False:
+                st = st + " _"
+            elif mtab[i][j] == 0:
+                st = st + "  "
+            elif mtab[i][j] == -1:
+                st = st + " *"
+            else:
+                st = st + " " + str(mtab[i][j])
+        print(st)
 
-mat_tab = cria_matriz(0)
-mat_esp = cria_matriz(False)
+#MAIN
+if __name__ == '__main__':
+    mat_tab = cria_matriz(0)
+    mat_esp = cria_matriz(False)
 
-coloca_bomba(mat_tab)
-coloca_numero(mat_tab)
-imprime(mat_tab)
+    coloca_bomba(mat_tab)
+    coloca_numeros(mat_tab)
+    #imprime(mat_tab)
+    mostra_campo(mat_tab, mat_esp)
 
-i = int(input("Informe a linha: "))
-j = int(input("Informe a coluna: "))
-mat_esp[i][j] = True
-
-if mat_tab[i][j] == -1:
-    print("Explodiu!")
-elif mat_tab[i][j] != 0:
+    i = int(input("Informe a linha: "))
+    j = int(input("Informe a coluna: "))
     mat_esp[i][j] = True
-else:
-    #Aqui achamos uma posicao 0
-    posicoes = []
-    posicoes.append([i, j])
-    while len(posicoes) != 0:
-        pos = posicoes.pop(0)
-        viz = retorna_vizinhos(pos[0], pos[1])
-        for [x, y] in viz:
-            if mat_esp[x][y] == False:
-                mat_esp[x][y] = True
-                if mat_tab[x][y] == 0:
-                    posicoes.append([x, y])
+
+    if mat_tab[i][j] == -1:
+        print("Explodiu!")
+    elif mat_tab[i][j] == 0:
+        #aqui achamos uma posicao 0
+        posicoes = []
+        posicoes.append( [i, j] )    
+        
+        while len(posicoes) != 0:
+            pos = posicoes.pop(0)
+            viz = retorna_vizinhos(pos[0], pos[1])
+            for [x, y] in viz:
+                if mat_esp[x][y] == False:
+                    mat_esp[x][y] = True
+                    if mat_tab[x][y] == 0:
+                        posicoes.append( [x, y] )
+
+    mostra_campo(mat_tab, mat_esp)
+    print("##############")
+    imprime(mat_tab)
